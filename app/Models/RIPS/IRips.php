2a2d434e77944474711689ba5996f8639e3d424c
desteban <esteban28c@hotmail.com>
2022-09-08 22:06:08 -0500

    cambiar el orden de las funcones de la interfaz de los RIPS

--- app/Models/RIPS/IRips.php
+++ app/Models/RIPS/IRips.php
@@ -4,15 +4,37 @@ namespace App\Models\RIPS;
 
 interface IRips
 {
-    public function subirDB();
 
-    public function obtenerDatos(): string;
-
-    public function agregarDatos(array $datos);
-
-    public function tipoRIPS(): string;
-
+    /**
+     * @return string con las colomnas de la tabla del rips
+     */
     public static function obtenerColumnasDB(): string;
 
+    /**
+     * @return string con las iniciales del tabla del rips (AC, AF, AM, ....)
+     */
+    public function tipoRIPS(): string;
+
+    /**
+     * esta funcion actualiza los atributos del objeto con los datos que se le pasan
+     * @param datos arreglo con los datos del archivo rips
+     */
+    public function agregarDatos(array $datos);
+
+    /**
+     * @return string datos del objeto separados por comas
+     */
+    public function obtenerDatos(): string;
+
+    /**
+     * crea la tabla temporal para vlaidar el archivo rips
+     * @param nombreTabla 
+     */
     public function crearTablas(string $nombreTabla);
+
+    /**
+     * @param datos arreglo que contiene toda la indormacion del RIPS
+     * @return bool el cual muestra el exito o fallo de guardar los datos en la base de datos
+     */
+    public function subirDB(array $datos = []);
 }
