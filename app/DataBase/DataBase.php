610ea9dfe2a18c7897594bb0e8556e94294de6c5
desteban <desteban28c@hotmail.com>
2022-09-22 15:50:59 -0500

    codigo para validar en base de datos

--- app/DataBase/DataBase.php
+++ app/DataBase/DataBase.php
@@ -4,4 +4,21 @@ namespace App\DataBase;
 
 class DataBase
 {
+    public static function validarNumeroFactura(string $tablaValidar, string $tablaValidadora): string
+    {
+        return "SELECT * FROM $tablaValidar
+        LEFT JOIN $tablaValidadora 
+        ON $tablaValidadora.codigoIps = $tablaValidar.codigoIps 
+        AND $tablaValidadora.numeroFactura = $tablaValidar.numeroFactura
+        WHERE $tablaValidadora.codigoIps is NULL;";
+    }
+
+    public static function validarIdentificacion(string $tablaValidar, string $tablaValidadora): string
+    {
+        return "SELECT * FROM $tablaValidar
+        LEFT JOIN $tablaValidadora 
+        ON $tablaValidadora.tipoIdentificacion = $tablaValidar.tipoIdentificacion 
+        AND $tablaValidadora.identificacion = $tablaValidar.identificacion
+        WHERE $tablaValidadora.identificacion is NULL;";
+    }
 }
